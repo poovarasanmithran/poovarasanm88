0f188d13fc983fc9b8380a4f8a5a6ffdf86e43ed
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-05 16:07:35 +0530

    5.2.con.c
    print the result

---
+++ 5.2.con.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(void) 
+{
+	char x[200],y[200];
+	printf("\n enter the 2 strings");
+	scanf("%s %s",&x,&y);
+	printf("\n concadenated string is %s%s",x,y);
+	return 0;
+}
