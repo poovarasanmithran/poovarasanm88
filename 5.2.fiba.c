50cb50906fbd8060377cdae59ad9a3f3571efbaf
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-05 15:53:56 +0530

    5.2.fiba.c
    to print the output

---
+++ 5.2.fiba.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a=0,b=1,n;
+int sum,i;
+scanf("%d",&n);
+for(i=0;i<n;++i)
+{
+sum=a+b;
+a=b;
+b=sum;
+printf("%d",sum);
+printf("**************************");
+}
+return 0;
+}
